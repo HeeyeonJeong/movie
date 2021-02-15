6763a7c5adba6dd9c43922283a145bcc5af1c2b3
HeeyeonJeong <junghy14971@gmail.com>
2021-02-15 16:13:16 +0900

    Feat: home reducer

--- src/store/modules/home.js
+++ src/store/modules/home.js
@@ -1,5 +1,7 @@
-import getTrendMovies from "../api/home";
+import * as homeAPI from "../api/home";
+import { initialState } from "./initialState";
 import { createPromiseThunk } from "../../lib/createPromiseThunk.js";
+import { handleAsyncActions } from "../../lib/handleAsyncActions";
 
 const GET_TREND_MOVIES = "home/GET_TREND_MOVIES";
 const GET_TREND_MOVIES_SUCCESS = "home/GET_TREND_MOVIES_SUCCESS";
@@ -7,5 +9,21 @@ const GET_TREND_MOVIES_ERROR = "home/GET_TREND_MOVIES_ERROR";
 
 export const getTrendMovies = createPromiseThunk(
   GET_TREND_MOVIES,
-  getTrendMovies
+  homeAPI.getTrendMovies
 );
+
+const getTrendMoviesReducer = handleAsyncActions(
+  GET_TREND_MOVIES,
+  "trendMovies"
+);
+
+export default function home(state = initialState, action) {
+  switch (action.type) {
+    case GET_TREND_MOVIES:
+    case GET_TREND_MOVIES_SUCCESS:
+    case GET_TREND_MOVIES_ERROR:
+      return getTrendMoviesReducer(state, action);
+    default:
+      return state;
+  }
+}
