4024252c8856b92560315117fd6c3ceb6a047708
HeeyeonJeong <junghy14971@gmail.com>
2021-03-03 13:48:35 +0900

    Fix: delete logger deployed

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,4 +1,4 @@
-import { createStore, applyMiddleware } from "redux";
+import { createStore, applyMiddleware, compose } from "redux";
 import { composeWithDevTools } from "redux-devtools-extension";
 import { createLogger } from "redux-logger";
 import ReduxThunk from "redux-thunk";
@@ -6,11 +6,13 @@ import rootReducer from "./modules/index";
 
 const logger = createLogger();
 
+const enhancer =
+  process.env.NODE_ENV === "production"
+    ? compose(applyMiddleware(ReduxThunk))
+    : composeWithDevTools(applyMiddleware(ReduxThunk, logger));
+
 function configureStore() {
-  const store = createStore(
-    rootReducer,
-    composeWithDevTools(applyMiddleware(ReduxThunk, logger))
-  );
+  const store = createStore(rootReducer, enhancer);
   return store;
 }
 
